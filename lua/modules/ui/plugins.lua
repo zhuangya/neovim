65f600dce82035d29776c724aac53a57e62d99f2
Ya Zhuang <zhuangya@users.noreply.github.com>
2022-07-16 16:22:12 +0800

    which key

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -2,12 +2,12 @@
 -- date: 2022-07-02
 -- License: MIT
 
-local plugin = require('core.pack').register_plugin
+local use = require('core.pack').register_plugin
 local conf = require('modules.ui.config')
 
-plugin({ 'glepnir/zephyr-nvim', config = conf.zephyr })
+use({ 'glepnir/zephyr-nvim', config = conf.zephyr })
 
-plugin({
+use({
     'goolord/alpha-nvim',
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = function ()
@@ -15,20 +15,30 @@ plugin({
     end
 })
 
-plugin({
+use({
   'kyazdani42/nvim-tree.lua',
   cmd = 'NvimTreeToggle',
   config = conf.nvim_tree,
   requires = 'kyazdani42/nvim-web-devicons',
 })
 
-plugin({ 'akinsho/nvim-bufferline.lua', config = conf.nvim_bufferline, requires = 'kyazdani42/nvim-web-devicons' })
-plugin({ 'lewis6991/gitsigns.nvim', config = conf.gitsigns, requires = 'nvim-lua/plenary.nvim' })
-plugin({
+use({ 'akinsho/nvim-bufferline.lua', config = conf.nvim_bufferline, requires = 'kyazdani42/nvim-web-devicons' })
+use({ 'lewis6991/gitsigns.nvim', config = conf.gitsigns, requires = 'nvim-lua/plenary.nvim' })
+use({
   'lukas-reineke/indent-blankline.nvim',
   config = conf.indent_blankline,
   requires = 'nvim-treesitter/nvim-treesitter',
   after = 'nvim-treesitter',
 })
-plugin({ 'sunjon/Shade.nvim' })
-plugin({ 'nxvu699134/vn-night.nvim' })
+use({ 'sunjon/Shade.nvim' })
+use({ 'nxvu699134/vn-night.nvim' })
+use {
+  "folke/which-key.nvim",
+  config = function()
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
