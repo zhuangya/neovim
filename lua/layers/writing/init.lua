516c692990d9cdd334735d472d8e47c8af19f069
Ya Zhuang <zhuangya@users.noreply.github.com>
2025-01-02 01:07:58 +0800

    introduce lazydev

--- lua/layers/writing/init.lua
+++ lua/layers/writing/init.lua
@@ -8,12 +8,21 @@ return {
     build = 'cargo build --release',
 
     opts = {
+      sources = {
+        default = {
+          'lazydev', 'lsp', 'path', 'snippets', 'buffer'
+        },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          }
+        }
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         -- nerd_font_variant = 'mono'
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
     },
   },
