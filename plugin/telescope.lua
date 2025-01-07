c75404453c4548a68254567dc5f959e08d9bc923
Ya Zhuang <zhuangya@users.noreply.github.com>
2025-01-07 17:27:10 +0800

    more telescope key mappings

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -35,8 +35,28 @@ if has_telescope then
     telescope.load_extension(ext)
   end
 
-  set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-  set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+  set("n", "<leader>fx", function()
+    builtin.colorscheme({
+      enable_preview = true,
+    })
+  end, {
+    desc = "colorscheme switcher",
+  })
+
+  set("n", "<leader>fm", builtin.marks, { desc = "Marks" })
+  set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
+  set("n", "<leader>fc", builtin.command_history, { desc = "Cmd History" })
+
+  set("n", "<leader>ff", function()
+    builtin.find_files({ hidden = true })
+  end, { desc = "Find Files" })
+  set("n", "<leader>fb", function()
+    builtin.buffers({
+      ignore_current_buffer = true,
+    })
+  end, { desc = "Buffers" })
+
+  set("n", "<leader>fj", builtin.jumplist, { desc = "Jump" })
   set("n", "<leader>fg", builtin.live_grep, { desc = "Grep" })
   set("n", "<leader>fe", "<cmd>Telescope egrepify<cr>", { desc = "EGrepIFY" })
   set("n", "<leader>fh", builtin.help_tags, { desc = "Help!" })
