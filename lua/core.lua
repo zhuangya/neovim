9d1c313d857f796fb42de50fdaf56e438572b19e
Ya Zhuang <zhuangya@users.noreply.github.com>
2025-01-11 12:39:33 +0800

    auto change dir to some kind of project root

--- lua/core.lua
+++ lua/core.lua
@@ -42,12 +42,51 @@ local ensure_lazy = function()
   vim.opt.rtp:prepend(lazypath)
 end
 
+local auto_chdir = function()
+  -- auto change to root directory
+  -- source: https://www.reddit.com/r/neovim/comments/zy5s0l/you_dont_need_vimrooter_usually_or_how_to_set_up/
+
+  -- Array of file names indicating root directory. Modify to your liking.
+  local root_names = { ".git", "Makefile", "gleam.toml" }
+
+  -- Cache to use for speed up (at cost of possibly outdated results)
+  local root_cache = {}
+
+  local set_root = function()
+    -- Get directory path to start search from
+    local path = vim.api.nvim_buf_get_name(0)
+    if path == "" then
+      return
+    end
+    path = vim.fs.dirname(path)
+
+    -- Try cache and resort to searching upward for root directory
+    local root = root_cache[path]
+    if root == nil then
+      local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
+      if root_file == nil then
+        return
+      end
+      root = vim.fs.dirname(root_file)
+      root_cache[path] = root
+    end
+
+    -- Set current directory
+    vim.fn.chdir(root)
+  end
+
+  local root_augroup = vim.api.nvim_create_augroup("MyAutoRoot", {})
+  vim.api.nvim_create_autocmd("BufEnter", { group = root_augroup, callback = set_root })
+end
+
 M.start = function()
   vim.loader.enable()
 
   basic_setup()
 
   ensure_lazy()
+
+  auto_chdir()
 
   require("lazy").setup("layers", {
     change_detection = {
