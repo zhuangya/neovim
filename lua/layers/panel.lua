d7d95179d50de6ca5875b1039017e2a12c97dd0d
Ya Zhuang <zhuangya@users.noreply.github.com>
2025-01-14 22:37:00 +0800

    enable snacks terminal

--- lua/layers/panel.lua
+++ lua/layers/panel.lua
@@ -68,6 +68,43 @@ return {
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scroll = { enabled = false },
+      terminal = {
+        enabled = true,
+        bo = {
+          filetype = "snacks_terminal",
+        },
+        wo = {},
+        keys = {
+          q = "hide",
+          gf = function(self)
+            local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+            if f == "" then
+              Snacks.notify.warn("No file under cursor")
+            else
+              self:hide()
+              vim.schedule(function()
+                vim.cmd("e " .. f)
+              end)
+            end
+          end,
+          term_normal = {
+            "<esc>",
+            function(self)
+              self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+              if self.esc_timer:is_active() then
+                self.esc_timer:stop()
+                vim.cmd("stopinsert")
+              else
+                self.esc_timer:start(200, 0, function() end)
+                return "<esc>"
+              end
+            end,
+            mode = "t",
+            expr = true,
+            desc = "Double escape to normal mode",
+          },
+        },
+      },
       statuscolumn = { enabled = false },
       words = { enabled = true },
     },
@@ -78,6 +115,13 @@ return {
           Snacks.notifier.show_history()
         end,
         desc = "notification history",
+      },
+      {
+        "<c-/>",
+        function()
+          Snacks.terminal()
+        end,
+        desc = "snacks terminal",
       },
     },
   },
