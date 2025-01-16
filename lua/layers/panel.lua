44e93e051d600ce34efb6e0a00a996299c6a1c46
Ya Zhuang <zhuangya@users.noreply.github.com>
2025-01-16 17:37:13 +0800

    fixup! add render-markdown plugin

--- lua/layers/panel.lua
+++ lua/layers/panel.lua
@@ -2,9 +2,7 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {},
   },
   {
